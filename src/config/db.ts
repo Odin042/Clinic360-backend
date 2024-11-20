db626aa7858f7ffefc0872201cb97e8b99a62cfc
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2024-11-20 19:12:04 -0300

    testando conexao com banco de dados

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,14 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from 'pg'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: Number(process.env.DB_PORT),
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
 
 export default pool;
