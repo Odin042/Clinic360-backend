eb46e6f0b63b867d2cf1a58b8cfa4c244d1e38f1
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2024-11-23 18:46:42 -0300

    ajusta o cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import dotenv from 'dotenv';
 
@@ -6,6 +7,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: ["https://clinicpro-ten.vercel.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   console.log(`Requisição recebida: ${req.method} ${req.url}`);
