0b9d09aab9ce9610571a7f84bb39a6b63fa814b5
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2024-11-30 22:01:51 -0300

    ajusta alguns dados e validacoes

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["https://clinicpro-ten.vercel.app", "http://localhost:5173"],
+  origin: (origin, callback) => {
+    const allowedOrigins = ["https://clinicpro-ten.vercel.app", "http://localhost:5173"];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Origem não permitida pelo CORS."));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-
+app.use(express.json());
 app.use((req, res, next) => {
-  console.log(`Requisição recebida: ${req.method} ${req.url}`);
+  console.log(`Requisição recebida: ${req.method} ${req.url}, Origem: ${req.headers.origin}`);
   next();
 });
-
-app.use(express.json());
 app.use(authRoutes);
 
 app.listen(PORT, () => {
