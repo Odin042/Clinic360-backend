32f232ce3f87756892f51d839efa87cecb897227
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2025-02-09 22:23:20 -0300

    Cria get de usuarios

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from "express"
-import  pool  from "../config/db"
-
+import pool from "../config/db"
 
 export const getUserByEmail = async (req: Request, res: Response) => {
   const { email } = req.query
@@ -9,13 +8,22 @@ export const getUserByEmail = async (req: Request, res: Response) => {
     return res.status(400).json({ error: "Email é obrigatório" })
   }
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  if (!emailRegex.test(email as string)) {
+    return res.status(400).json({ error: "Email inválido" })
+  }
+
   try {
     const result = await pool.query("SELECT * FROM usuario WHERE email = $1", [email])
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "Usuário não encontrado" })
     }
 
-    res.json(result.rows[0])
+ 
+    const user = result.rows[0]
+    delete user.password
+
+    res.json(user);
   } catch (error) {
     console.error("Erro ao buscar usuário:", error)
     res.status(500).json({ error: "Erro no servidor" })
