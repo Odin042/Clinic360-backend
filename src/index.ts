c8c0b0c4b8f49838da0121009b1a930e25ec7621
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2025-06-30 16:54:01 -0300

    Ajusta cors para credentials true

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-
 const corsOptions = {
-  origin: '*',
+  origin: [
+    'https://clinic360pro-git-homolog-clinic360pro.vercel.app',
+    'https://clinic360pro-git-master-clinic360pro.vercel.app',
+    'https://clinic360pro.com.br',
+    'http://localhost:3000'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -18,7 +22,6 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
 app.use(express.json())
 app.use(authRoutes)
 
