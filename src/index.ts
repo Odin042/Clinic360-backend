748f6a1a420359bd8a936352d650819300c4a4a6
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2025-07-01 17:50:02 -0300

    ajusta cors

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,9 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+
 const corsOptions = {
-  origin: [
-    'https://clinic360pro-git-homolog-clinic360pro.vercel.app',
-    'https://clinic360pro-git-master-clinic360pro.vercel.app',
-    'https://clinic360pro.com.br',
-    'http://localhost:3000'
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
