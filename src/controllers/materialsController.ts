9a36fa40acf4a3ff3ff9c67008443b2c0c1afa0c
Guilherme Fernandes <guilherme.silva.stefanini@elo.com.br>
2025-07-07 19:36:51 -0300

    Ajusta chamada de estoque para criar vinculo com usuario

--- src/controllers/materialsController.ts
+++ src/controllers/materialsController.ts
@@ -2,22 +2,24 @@ import type { RequestHandler } from 'express'
 import pool from '../config/db'
 
 export const createMaterial: RequestHandler = async (req, res) => {
-  const { name, category, unit, measures ,stock } = req.body
+  const { name, category, unit, measures, stock, expiration, user_id } = req.body
 
-  if (!name || !category || !unit || !measures|| stock === undefined) {
+  if (!name || !category || !unit || !measures || stock === undefined || !expiration || !user_id) {
     return res.status(400).json({ error: 'Todos os campos são obrigatórios' })
   }
 
   try {
     const result = await pool.query(
-      'INSERT INTO materials (name, category, unit, measures,stock ) VALUES ($1, $2, $3, $4,  $5) RETURNING *',
-      [name, category, unit, measures, stock]
+      `INSERT INTO materials (name, category, unit, measures, stock, user_id, expiration)
+       VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+      [name, category, unit, measures, stock, user_id, expiration]
     )
     res.status(201).json(result.rows[0])
   } catch (error) {
     res.status(500).json({ error: 'Erro ao criar material' })
   }
 }
+
 
 export const listMaterials: RequestHandler = async (_req, res) => {
   try {
@@ -30,12 +32,12 @@ export const listMaterials: RequestHandler = async (_req, res) => {
 
 export const updateMaterial: RequestHandler = async (req, res) => {
   const { id } = req.params
-  const { name, category, unit, measures, stock } = req.body
+  const { name, category, unit, measures, stock, user_id } = req.body
 
   try {
     const result = await pool.query(
-      'UPDATE materials SET name = $1, category = $2, unit = $3, measures  = $4, stock = $5 WHERE id = $6 RETURNING *',
-      [name, category, unit, measures, stock, id]
+      'UPDATE materials SET name = $1, category = $2, unit = $3, measures = $4, stock = $5, user_id = $6 WHERE id = $7 RETURNING *',
+      [name, category, unit, measures, stock, user_id, id]
     )
     if (result.rowCount === 0) {
       return res.status(404).json({ error: 'Material não encontrado' })
